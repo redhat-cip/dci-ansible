abdd83794da5e475f7101a78d1f7bc72564b7ddf
Gonéri Le Bouder <goneri@redhat.com>
2017-03-06 13:18:53 -0500

    Ability to use a configuration file
    Ansible does not export the DCI_* environment variables. We use a
    configuration file to pass the DCI credentials.
    
    Change-Id: Ie6dbb480f2047a11e1a8985f6c86e563ea66447c

--- modules/dci_team.py
+++ modules/dci_team.py
@@ -14,6 +14,7 @@
 from ansible.module_utils.basic import *
 
 import os
+import yaml
 
 try:
     from dciclient.v1.api import context as dci_context
@@ -96,19 +97,35 @@ RETURN = '''
 '''
 
 
-def get_details(module):
-    """Method that retrieves the appropriate credentials. """
+def get_config(module):
+    from_file = {}
+    if os.path.exists('/etc/dci/dci.yaml'):
+        with open('/etc/dci/dci.yaml') as fd:
+            from_file = yaml.load(fd)
 
-    login_list = [module.params['dci_login'], os.getenv('DCI_LOGIN')]
-    login = next((item for item in login_list if item is not None), None)
-
-    password_list = [module.params['dci_password'], os.getenv('DCI_PASSWORD')]
-    password = next((item for item in password_list if item is not None), None)
-
-    url_list = [module.params['dci_cs_url'], os.getenv('DCI_CS_URL')]
-    url = next((item for item in url_list if item is not None), 'https://api.distributed-ci.io')
-
-    return login, password, url
+    return {
+        'login':
+            module.params.get('dci_login')
+            or os.getenv('DCI_LOGIN')
+            or from_file.get('login'),
+        'password':
+            module.params.get('dci_password')
+            or os.getenv('DCI_PASSWORD')
+            or from_file.get('password'),
+        'cs_url':
+            module.params.get('dci_cs_url')
+            or os.getenv('DCI_CS_URL')
+            or from_file.get('cs_url')
+            or 'https://api.distributed-ci.io',
+        'remoteci':
+            module.params.get('remoteci')
+            or os.getenv('DCI_REMOTECI')
+            or from_file.get('remoteci'),
+        'topic':
+            module.params.get('topic')
+            or os.getenv('DCI_TOPIC')
+            or from_file.get('topic')
+        }
 
 
 def module_params_empty(module_params):
@@ -138,15 +155,17 @@ def main():
             notification=dict(type='bool'),
         ),
     )
+    config = get_config(module)
 
     if not dciclient_found:
-        module.fail_json(msg='The python dciclient module is required')
+        module.fail_json(msg='the python dciclient module is required')
 
-    login, password, url = get_details(module)
-    if not login or not password:
+    if not set(['login', 'password']).issubset(config):
         module.fail_json(msg='login and/or password have not been specified')
 
-    ctx = dci_context.build_dci_context(url, login, password, 'Ansible')
+    ctx = dci_context.build_dci_context(
+        config['cs_url'], config['login'],
+        config['password'], 'ansible')
 
     # Action required: List all teams
     # Endpoint called: /teams GET via dci_team.list()
