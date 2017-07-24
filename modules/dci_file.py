876d61e6bf1d63fb5f6f43a7df08913e7f770fa9
François Charlier <francois.charlier@redhat.com>
2017-07-24 18:39:59 +0200

    Pass a stream to dci_file.create()
    dci_file.create() can be passed a stream instead of the full file
    content to reduce memory usage when uploading large files.
    
    Fixes bug #5
    
    Change-Id: If823f4aae008c2e86f103421a85d271426b32086

--- modules/dci_file.py
+++ modules/dci_file.py
@@ -183,8 +183,8 @@ def main():
 
         if module.params['path']:
             try:
-                content = open(module.params['path'], 'r').read()
-            except IOError as e:
+                content = open(module.params['path'], 'r')
+            except IOError:
                 module.fail_json(msg='The path specified cannot be read')
         else:
             content = module.params['content']
