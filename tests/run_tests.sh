86d93dacffd467faa329e82607f76c2b36c01aa4
Yanis Guenane <yguenane@redhat.com>
2017-10-19 10:59:30 +0200

    README.md: Introduce new README.md
    Change-Id: I55556e756cb0101223e1334b46c8635f99bef8f7

---
+++ tests/run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eux
+
+export DCI_LOGIN='admin'
+export DCI_PASSWORD='admin'
+export DCI_CS_URL='http://127.0.0.1:5000'
+
+modules='dci_user dci_team'
+
+exit 0
+
+for module in $modules; do
+    ansible-playbook $module/playbook.yml -vvv
+    dcictl purge
+done
