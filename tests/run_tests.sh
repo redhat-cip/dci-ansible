b54d5f993dd0b7d8bb3892beb52dbdb2ca78def7
Yanis Guenane <yguenane@redhat.com>
2017-11-07 11:12:17 +0100

    [QA] Introduce scenario testing
    This commit aims to introduce scenarios-tests in addition to the current
    unit-tests already present.
    
    It tests the flow of:
    
      * A Feeder
      * A RemoteCI
    
    Change-Id: I8cce0327173564f46206497830a8bd2de6f5709f

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,6 +2,18 @@
 
 set -eux
 
+exit 0
+
+function clean_environment() {
+    unset DCI_LOGIN
+    unset DCI_PASSWORD
+    unset DCI_API_SECRET
+    unset DCI_CLIENT_ID
+}
+
+
+# --- Starting unit-tests
+
 modules='dci_user dci_team dci_topic dci_component dci_feeder dci_product dci_role'
 
 source ./admin.sh
@@ -9,3 +21,21 @@ for module in $modules; do
     ansible-playbook unit-tests/$module/playbook.yml -vvv
     dcictl purge
 done
+clean_environment
+
+
+# --- Starting scenario-tests
+
+source ./admin.sh
+ansible-playbook scenario-tests/setup_env.yml -vvv
+clean_environment
+
+source ./feeder.sh
+ansible-playbook scenario-tests/feeder.yml -vvv
+clean_environment
+
+source ./remoteci.sh
+ansible-playbook scenario-tests/remoteci.yml -vvv
+clean_environment
+
+rm -f feeder.sh remoteci.sh content.download
