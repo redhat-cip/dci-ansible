0208c7097a2a3d77154f858cd3a43321781f2b06
Yanis Guenane <yguenane@redhat.com>
2017-11-14 15:48:44 +0100

    qa: Moving creds from script to file
    Change-Id: I92c77bffa01590a7d0b6cd23c13aeb5096d62ab1

---
+++ tests/admin.sh
@@ -0,0 +1,3 @@
+export DCI_LOGIN='admin'
+export DCI_PASSWORD='admin'
+export DCI_CS_URL='http://127.0.0.1:5000'
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,14 +2,9 @@
 
 set -eux
 
-export DCI_LOGIN='admin'
-export DCI_PASSWORD='admin'
-export DCI_CS_URL='http://127.0.0.1:5000'
-
 modules='dci_user dci_team dci_topic dci_component dci_feeder dci_product dci_role'
 
-exit 0
-
+source ./admin.sh
 for module in $modules; do
     ansible-playbook unit-tests/$module/playbook.yml -vvv
     dcictl purge
