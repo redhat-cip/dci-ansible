redhat-cip/dci-ansible

commit 617e803bd63399fa58072707dda2913bb9295eab

callback: simplify the job creation log
- Read the job id from the output of the dci_job module. We don't read
  it from the job_informations variable anymore.
- Bail out for the rest of the cases if we don't have a job_id

Change-Id: I5f423ac3676ebc1f299506671a4846edfa77320e

callback/dci.py
@@ -229,10 +229,19 @@ class CallbackModule(CallbackBase):
         if (result._task.action == 'dci_job' and (
                 result._result['invocation']['module_args']['upgrade'] or
                 result._result['invocation']['module_args']['update'])):
-            self._job_id = result._result['job']['id']
 
+            self._job_id = result._result['job']['id']
             ns = dci_jobstate.create(self._dci_context, status='pre-run',
                                      comment='starting the update/upgrade',
+                                     job_id=self._job_id).json()
+            self._jobstate_id = ns['jobstate']['id']
+        elif (result._task.action == 'dci_job' and
+              result._result['invocation']['module_args']['topic'] and
+              not result._result['invocation']['module_args']['id']):
+
+            self._job_id = result._result['job']['id']
+            ns = dci_jobstate.create(self._dci_context, status='new',
+                                     comment='start up',
                                      job_id=self._job_id).json()
             self._jobstate_id = ns['jobstate']['id']
 
@@ -245,6 +254,10 @@ class CallbackModule(CallbackBase):
             self.post_message(result, output)
 
     def v2_runner_on_unreachable(self, result):
+
+        if not self._job_id:
+            return
+
         super(CallbackModule, self).v2_runner_on_unreachable(result)
         new_state = dci_jobstate.create(
             self._dci_context,
@@ -258,6 +271,9 @@ class CallbackModule(CallbackBase):
         """Event executed after each command when it fails. Get the output
         of the command and create a failure jobstate and a file associated.
         """
+
+        if not self._job_id:
+            return
 
         super(CallbackModule, self).v2_runner_on_failed(result, ignore_errors)
 
@@ -288,6 +304,9 @@ class CallbackModule(CallbackBase):
         the current jobstate id.
         """
 
+        if not self._job_id:
+            return
+
         def _get_comment(play):
             """ Return the comment for the new jobstate
 
@@ -311,23 +330,8 @@ class CallbackModule(CallbackBase):
 
         super(CallbackModule, self).v2_playbook_on_play_start(play)
         comment = _get_comment(play)
-        fact_cache = play._variable_manager._nonpersistent_fact_cache
-        if play.get_vars():
-            self._current_status = play.get_vars().get('dci_status')
-            if 'dci_mime_type' in play.get_vars():
-                self._mime_type = play.get_vars()['dci_mime_type']
-            else:
-                self._mime_type = 'text/plain'
-
-        if self._current_status:
-            if not self._job_id:
-                for hostvar in fact_cache:
-                    if 'job_informations' in fact_cache[hostvar]:
-                        self._job_id = \
-                            fact_cache[hostvar]['job_informations']['id']
-                        break
-
-            ns = dci_jobstate.create(self._dci_context, status=self._current_status,
-                                     comment=comment,
-                                     job_id=self._job_id).json()
-            self._jobstate_id = ns['jobstate']['id']
+        ns = dci_jobstate.create(self._dci_context,
+                                 self._current_status,
+                                 comment=comment,
+                                 job_id=self._job_id).json()
+        self._jobstate_id = ns['jobstate']['id']

tests/scenario-tests/remoteci.yml
@@ -44,9 +44,9 @@
         embed: files,jobstates
       register: job_informations
 
-    - name: Verify that job has 4 files attached
+    - name: Verify that job has 6 files attached
       assert:
-        that: job_informations.job.files|length == 4
+        that: job_informations.job.files|length == 6
 
 - hosts: localhost
   tasks:

commit e3f8c1fbf89bca03136062cb98dc5e5c85512488

fix the jobstate creation
Because of a regression introduced in 617e803bd63399fa58072707dda2913bb9295eab
the jobstate were not created anymore in v2_playbook_on_play_start().

This patch fixes the problem and simplify the create of new jobstate. They
are now always created in the create_jobstate() method.

Change-Id: I9b5f42acd8a4f52c50311aa6139a46dc16bedc52

callback/dci.py
@@ -201,6 +201,18 @@ class CallbackModule(CallbackBase):
             self._dci_context,
             **kwargs)
 
+    def create_jobstate(self, comment, status=None):
+        if status:
+            self._current_status = status
+
+        r = dci_jobstate.create(
+            self._dci_context,
+            status=self._current_status,
+            comment=comment,
+            job_id=self._job_id)
+        ns = r.json()
+        self._jobstate_id = ns['jobstate']['id']
+
     def task_name(self, result):
         """Ensure we alway return a string"""
         name = result._task.get_name()
@@ -231,19 +243,14 @@ class CallbackModule(CallbackBase):
                 result._result['invocation']['module_args']['update'])):
 
             self._job_id = result._result['job']['id']
-            ns = dci_jobstate.create(self._dci_context, status='pre-run',
-                                     comment='starting the update/upgrade',
-                                     job_id=self._job_id).json()
-            self._jobstate_id = ns['jobstate']['id']
+            self.create_jobstate(comment='starting the update/upgrade', status='pre-run')
+
         elif (result._task.action == 'dci_job' and
               result._result['invocation']['module_args']['topic'] and
               not result._result['invocation']['module_args']['id']):
 
             self._job_id = result._result['job']['id']
-            ns = dci_jobstate.create(self._dci_context, status='new',
-                                     comment='start up',
-                                     job_id=self._job_id).json()
-            self._jobstate_id = ns['jobstate']['id']
+            self.create_jobstate(comment='starting the update/upgrade', status='new')
 
         try:
             output = Formatter().format(result)
@@ -259,12 +266,7 @@ class CallbackModule(CallbackBase):
             return
 
         super(CallbackModule, self).v2_runner_on_unreachable(result)
-        new_state = dci_jobstate.create(
-            self._dci_context,
-            status='failure',
-            comment=self.task_name(result),
-            job_id=self._job_id).json()
-        self._jobstate_id = new_state['jobstate']['id']
+        self.create_jobstate(status='failure', comment=self.task_name(result))
         self.post_message(result, result._result['msg'])
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
@@ -291,12 +293,7 @@ class CallbackModule(CallbackBase):
         else:
             status = 'failure'
 
-        new_state = dci_jobstate.create(
-            self._dci_context,
-            status=status,
-            comment=self.task_name(result),
-            job_id=self._job_id).json()
-        self._jobstate_id = new_state['jobstate']['id']
+        self.create_jobstate(status=status, comment=self.task_name(result))
         self.post_message(result, output)
 
     def v2_playbook_on_play_start(self, play):
@@ -330,8 +327,6 @@ class CallbackModule(CallbackBase):
 
         super(CallbackModule, self).v2_playbook_on_play_start(play)
         comment = _get_comment(play)
-        ns = dci_jobstate.create(self._dci_context,
-                                 self._current_status,
-                                 comment=comment,
-                                 job_id=self._job_id).json()
-        self._jobstate_id = ns['jobstate']['id']
+        self.create_jobstate(
+            comment=comment,
+            status=play.get_vars().get('dci_status'))

tests/scenario-tests/remoteci.yml
@@ -48,6 +48,26 @@
       assert:
         that: job_informations.job.files|length == 6
 
+    - debug:
+        var: job_informations.job.jobstates
+
+    - name: Check the jobstates are all created
+      assert:
+        that: job_informations.job.jobstates|length == 5
+
+    - name: And the jobstates.status are set (1/3)
+      assert:
+        that: job_informations.job.jobstates[0].status == "new"
+
+    - name: And the jobstates.status are set (2/3)
+      assert:
+        that: job_informations.job.jobstates[2].status == "post-run"
+
+    - name: And the jobstates.status are set (3/3)
+      assert:
+        that: job_informations.job.jobstates[-1].status == "success"
+
+
 - hosts: localhost
   tasks:
     - name: Request an update job