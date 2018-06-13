1857dc1162f485d006a92a24b8b43d40d8e741f6
Gonéri Le Bouder <goneri@redhat.com>
2018-06-13 11:39:50 -0400

    test/callback: improve the file module
    Test the actual behavior of the module instead of Ansible's argument
    parser.
    
    Change-Id: Ib2e708cc1637de9dbe41a169577365219faf4936

--- tests/test_callback_formatter.py
+++ tests/test_callback_formatter.py
@@ -150,26 +150,21 @@ def test_debug_failure(capsys):
     assert outerr.out == expectation
 
 
-def test_file_success(capsys):
-    args = 'path=/etc/aliases state=present'
+def test_file_success(capsys, tmpdir):
+    args = 'path=%s/aliases state=touch' % tmpdir
     run_task({'action': {'module': 'file', 'args': args}})
-    expectation = ("value of state must be one of: file, directory, link, "
-                   "hard, touch, absent, got: present\nNone: None (changed: "
-                   "False)\n\n")
     outerr = capsys.readouterr()
     assert not outerr.err
-    assert outerr.out == expectation
+    assert 'changed: True' in outerr.out
 
 
 def test_file_failure(capsys):
-    args = 'path=/proc/1/bob state=present'
+    args = 'path=/proc/1/bob state=touch'
     run_task({'action': {'module': 'file', 'args': args}})
-    expectation = ('value of state must be one of: file, directory, link, '
-                   'hard, touch, absent, got: present\nNone: None (changed: '
-                   'False)\n\n')
+    expectation = 'No such file or directory'
     outerr = capsys.readouterr()
     assert not outerr.err
-    assert outerr.out == expectation
+    assert expectation in outerr.out
 
 
 def test_find_success(capsys):
