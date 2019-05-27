7fc38d088900eb4aca9e49a3d186bf107119165b
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2019-05-27 15:19:49 +0200

    fix: ansible 2.8 is more verbose
    ansible 2.8 run more internal tasks thus leading
    the callback plugin to create more files in
    the control server, 10 instead of 6.
    
    Also, the class TaskQueueManager does not
    have an 'options' parameter. We provide
    the connection='local' parameter directly
    in the Play class.
    
    Change-Id: I1b0965606ce61abdd737249789ce2c4df7dc4ca4

--- tests/test_callback_formatter.py
+++ tests/test_callback_formatter.py
@@ -12,12 +12,6 @@ import sys
 
 
 formatter = dci_callback.Formatter()
-Options = namedtuple('Options', ['connection', 'module_path', 'forks',
-                                 'become', 'become_method', 'become_user',
-                                 'check', 'diff'])
-options = Options(connection='local', module_path=['modules'], forks=1,
-                  become=None, become_method=None, become_user=None,
-                  check=False, diff=False)
 loader = DataLoader()
 passwords = {'vault_pass': 'secret'}
 inventory = InventoryManager(loader=loader, sources='localhost,')
@@ -25,7 +19,7 @@ variable_manager = VariableManager(loader=loader, inventory=inventory)
 
 # NOTE(Gonéri): In case we are in a virtualenv, we must reuse the same Python
 # interpreter to be able to reach the dciclient lib.
-variable_manager.extra_vars = {'ansible_python_interpreter': sys.executable}
+variable_manager._extra_vars = {'ansible_python_interpreter': sys.executable}  # noqa
 
 
 def run_task(task):
@@ -33,6 +27,7 @@ def run_task(task):
         name="Ansible Play",
         hosts='localhost',
         gather_facts='no',
+        connection='local',
         tasks=[task])
 
     play = Play().load(play_source)
@@ -50,7 +45,6 @@ def run_task(task):
         inventory=inventory,
         variable_manager=variable_manager,
         loader=loader,
-        options=options,
         passwords=passwords,
         stdout_callback=results_callback)
     tqm.run(play)
@@ -189,8 +183,8 @@ def test_find_failure(capsys):
 
 def test_firewalld_failure(capsys):
     run_task({'action': {'module': 'firewalld', 'args': 'port=80 state=BOB'}})
-    exceptation = ('value of state must be one of: enabled, disabled, '
-                   'present, absent, got: BOB\n\n')
+    exceptation = ('value of state must be one of: absent, disabled, '
+                   'enabled, present, got: BOB\n\n')
     outerr = capsys.readouterr()
     assert not outerr.err
     assert outerr.out == exceptation
