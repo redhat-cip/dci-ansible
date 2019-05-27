c1b84c6a3bc26d07a1a53c06e9a991adc2e4e633
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2019-05-27 15:19:49 +0200

    fix: ansible 2.8 is more verbose
    thus running more tasks this leads to
    10 files created instead of 6.
    
    Change-Id: I1b0965606ce61abdd737249789ce2c4df7dc4ca4

--- tests/test_callback_formatter.py
+++ tests/test_callback_formatter.py
@@ -25,7 +25,7 @@ variable_manager = VariableManager(loader=loader, inventory=inventory)
 
 # NOTE(Gonéri): In case we are in a virtualenv, we must reuse the same Python
 # interpreter to be able to reach the dciclient lib.
-variable_manager.extra_vars = {'ansible_python_interpreter': sys.executable}
+variable_manager._extra_vars = {'ansible_python_interpreter': sys.executable}  # noqa
 
 
 def run_task(task):
@@ -33,6 +33,7 @@ def run_task(task):
         name="Ansible Play",
         hosts='localhost',
         gather_facts='no',
+        connection='local',
         tasks=[task])
 
     play = Play().load(play_source)
@@ -50,7 +51,6 @@ def run_task(task):
         inventory=inventory,
         variable_manager=variable_manager,
         loader=loader,
-        options=options,
         passwords=passwords,
         stdout_callback=results_callback)
     tqm.run(play)
